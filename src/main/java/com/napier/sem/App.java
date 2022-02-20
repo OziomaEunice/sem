87e7f42258e550dfe6c45d73357af0ea1b0eac04
Ozioma Eunice Onwubiko <onwubikooziomaeunice@gmail.com>
2022-02-20 22:09:43 +0000

    file main

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -133,4 +133,51 @@ public class App
             }
         }
     }
+
+    /*
+    Get employee method
+     */
+    public Employee getEmployee(int ID){
+
+        try
+        {
+            /*
+            There's an error it gives me (con in red) but looking
+            at solutions on Google it appears that there is a statement
+            to be made before calling it...
+
+            Connection con = DriverManager.getConnection();
+             */
+            //Create an SQL statement
+            Statement stmt = con.createStatement();
+
+            //Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name " +
+                            "FROM employees " +
+                            "WHERE emp_no = " + ID;
+
+            //Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            //Return new employee if valid.
+            //Check one is returned
+            if(rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
 }
